a26c1b8c0f8cfb0d7843eabb5391ff1714afa671
priyanshu-sharma <prynsh1112@gmail.com>
2026-05-12 16:10:07 -0400

    fix deploy

--- backend_api/content_management/api/contact_api.py
+++ backend_api/content_management/api/contact_api.py
@@ -4,7 +4,10 @@ from databases.primary.content import contact
 
 def get_contact() -> Contact:
     try:
-        return Contact.select()[0]
+        data = Contact.select()
+        if data:
+            return data[0]
+        raise Exception("No contact found")
     except Exception:
         return Contact(
             **{**contact.CONTACT, "created_by": "system", "updated_by": "system"}
--- backend_api/content_management/api/profile_api.py
+++ backend_api/content_management/api/profile_api.py
@@ -4,7 +4,10 @@ from databases.primary.content import profile
 
 def get_profile() -> Profile:
     try:
-        return Profile.select()[0]
+        data = Profile.select()
+        if data:
+            return data[0]
+        raise Exception("No profile found")
     except Exception:
         return Profile(
             **{**profile.PROFILE, "created_by": "system", "updated_by": "system"}
